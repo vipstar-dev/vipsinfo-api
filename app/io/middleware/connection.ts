7ab0ebea161e5b86d2676d5a9a4350fcee941b38
y-chan <y-chan@y-chan.dev>
2021-05-04 00:01:08 +0900

    rewrite app io middleware connection

---
+++ app/io/middleware/connection.ts
@@ -0,0 +1,14 @@
+import { Context } from 'egg'
+
+export default function connection() {
+  return async (ctx: Context, next: CallableFunction) => {
+  const { app, socket } = ctx
+    let interval = setInterval(() => {
+      if (app.blockchainInfo.tip) {
+        socket.emit('block-height', app.blockchainInfo.tip.height)
+        clearInterval(interval)
+      }
+    }, 0)
+    await next()
+  }
+}

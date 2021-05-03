bf3a8ffec096b93b5e05e0b8a98bff0f3211951b
y-chan <y-chan@y-chan.dev>
2021-05-03 21:22:19 +0900

    rewrite app middleware ratelimit

---
+++ app/middleware/ratelimit.ts
@@ -0,0 +1,29 @@
+import { Application } from 'egg'
+import Ratelimit, { MiddlewareOptions } from 'koa-ratelimit'
+
+interface WhiteListAnyMiddlewareOptions extends MiddlewareOptions {
+  whitelist: any
+}
+
+interface CustomMiddlewareOptions extends WhiteListAnyMiddlewareOptions {
+  whitelist: string[]
+}
+
+export default function ratelimit(
+  options: CustomMiddlewareOptions,
+  app: Application
+) {
+  return Ratelimit({
+    ...options,
+    id: (ctx) =>
+      `${app.name}-${
+        ctx.get('cf-conecting-ip') || ctx.get('x-forwarded-for') || ctx.ip
+      }`,
+    whitelist:
+      options.whitelist &&
+      ((ctx) =>
+        options.whitelist.includes(
+          ctx.get('cf-connecting-ip') || ctx.get('x-forwarded-for') || ctx.ip
+        ) || options.whitelist.includes(ctx.get('application-id'))),
+  })
+}
